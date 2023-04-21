244631871cdb8f9ca95ff6da36f102411cb1eb07
ZeeShan <m.zeeshan1600@gmail.com>
2023-04-21 16:38:14 +0500

    Changes

--- js/script.js
+++ js/script.js
@@ -1,30 +1,6 @@
 
 
-const swiper = new Swiper('.swiper', {
-    // Optional parameters
-    direction: 'horizontal',
-    loop: true,
-  
-    // If we need pagination
-    pagination: {
-      el: '.swiper-pagination',
-    },
-  
-    // Navigation arrows
-    navigation: {
-      nextEl: ' .swiper-button-next ',
-      prevEl: ' .swiper-button-prev',
-    },
-  
-    // And if we need scrollbar
-    scrollbar: {
-      el: '.swiper-scrollbar',
-    },
-  });
-
-  
-
-const swiper1 = new Swiper('.swiper1', {
+const writerSwiper = new Swiper('.myWriterSwiper', {
   // Optional parameters
   direction: 'horizontal',
   loop: true,
@@ -40,8 +16,25 @@ const swiper1 = new Swiper('.swiper1', {
     prevEl: ' .swiper-button-prev',
   },
 
-  // And if we need scrollbar
-  scrollbar: {
-    el: '.swiper-scrollbar',
+});
+
+
+
+const swiper1 = new Swiper('.mySwiper', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+    type: 'bullets',
+  },
+
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ' .swiper-button-next ',
+    prevEl: ' .swiper-button-prev',
   },
 });
